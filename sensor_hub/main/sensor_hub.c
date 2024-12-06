f854bb0519c7af67a5f0ddf553b105edfabbabfb
pl450b <pl450b@outlook.com>
2024-12-06 01:22:52 +0000

    UART2 working in sensor hub's task

--- sensor_hub/main/sensor_hub.c
+++ sensor_hub/main/sensor_hub.c
@@ -261,25 +261,24 @@ void app_main(void)
     /* Initialize UART1 */ 
     const uart_port_t uart_num = UART_NUM;
     uart_config_t uart_config = {
-      .baud_rate = 115200,
-      .data_bits = UART_DATA_8_BITS,
-      .parity = UART_PARITY_DISABLE,
-      .stop_bits = UART_STOP_BITS_1,
-      .flow_ctrl = UART_HW_FLOWCTRL_CTS_RTS,
-      .rx_flow_ctrl_thresh = 122,
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity    = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_DEFAULT,
     };
+
+    const int uart_buffer_size = (1024 * 2);
+    ESP_ERROR_CHECK(uart_driver_install(UART_NUM, uart_buffer_size, 0, 0, NULL, 0));
     ESP_ERROR_CHECK(uart_param_config(uart_num, &uart_config));
     ESP_ERROR_CHECK(uart_set_pin(UART_NUM, 17, 16, 18, 19));
     // Setup UART buffered IO with event queue
-    const int uart_buffer_size = (1024 * 2);
-    QueueHandle_t uart_queue;
+    // QueueHandle_t uart_queue;
     // Install UART driver using an event queue here
     //ESP_ERROR_CHECK(uart_driver_install(UART_NUM, uart_buffer_size, uart_buffer_size, 10, &uart_queue, 0));
-    ESP_ERROR_CHECK(uart_driver_install(UART_NUM, uart_buffer_size, 0, 0, NULL, 0));
     //Start TCP server task
     xTaskCreate(tcp_server_task, "tcp_server", 4096, (void*)AF_INET, 5, NULL);
 
     xTaskCreate(uart_send_test, "uart test", 4096, NULL, 5, NULL);
-
-
 }
