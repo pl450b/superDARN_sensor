662a72374157a2748f2896a5b7a0ff72e105c97c
pl450b <pl450b@outlook.com>
2025-02-16 20:36:21 +0000

    updated for new cpp and monitor window

--- tmux_env.sh
+++ tmux_env.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-MY_IDF_PATH="~/esp/esp-idf"
 SESSION="superDARN"
 SESSIONEXISTS=$(tmux list-sessions | grep $SESSION)
 
@@ -11,20 +10,26 @@ then
   
   # Setup sensor unit environment
   tmux rename-window -t 0 'Sensor_Unit'
-  tmux send-keys -t 'Sensor_Unit' 'cd ./sensor_unit/main && nvim sensor_unit.c' C-m
+  tmux send-keys -t 'Sensor_Unit' 'cd ./sensor_unit/main && nvim main.cpp' C-m
   tmux split-window -h
-  tmux send-keys -t 'Sensor_Unit' 'source $MY_IDF_PATH/export.sh' C-m
-  tmux send-keys -t 'Sensor_Unit' './sensor_unit/main && ls' C-m
+  tmux send-keys -t 'Sensor_Unit' 'source ~/esp/esp-idf/export.sh' C-m
+  tmux send-keys -t 'Sensor_Unit' 'cd ./sensor_unit && idf.py build' C-m
 
   # Setup sensor hub environment
   tmux new-window -t $SESSION:1 -n 'Sensor_Hub'
-  tmux send-keys -t 'Sensor_Hub' 'cd ./sensor_hub/main && nvim sensor_hub.c' C-m
+  tmux send-keys -t 'Sensor_Hub' 'cd ./sensor_hub/main && nvim main.cpp' C-m
   tmux split-window -h
-  tmux send-keys -t 'Sensor_Hub' 'source $MY_IDF_PATH/export.sh' C-m
-  tmux send-keys -t 'Sensor_Hub' './sensor_hub/main && ls' C-m
+  tmux send-keys -t 'Sensor_Hub' 'source ~/esp/esp-idf/export.sh' C-m
+  tmux send-keys -t 'Sensor_Hub' 'cd ./sensor_hub && idf.py build' C-m
   
+  # Setup flashing/monitor window
+  tmux new-window -t $SESSION:2 -n 'FnM'
+  tmux send-keys -t 'FnM' 'cd ./sensor_hub && source ~/esp/esp-idf/export.sh' C-m
+  tmux split-window -h
+  tmux send-keys -t 'FnM' 'cd ./sensor_unit && source ~/esp/esp-idf/export.sh' C-m
+
   # Setup host computer program environment
-  tmux new-window -t $SESSION:2 -n 'Host_Comp'
+  tmux new-window -t $SESSION:3 -n 'Host_Comp'
   tmux send-keys -t 'Host_Comp' 'cd ./host_computer && nvim main.cpp' C-m
   tmux split-window -h
 fi 
